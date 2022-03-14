99d3f65f0fa329ca66783b56ee07b354af035065
Davide Manini <davide@manini.cloud>
2022-03-14 11:24:35 +0100

    Fix upload_configuration.sh

--- upload_configuration.sh
+++ upload_configuration.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-rsync -a -v -e ssh --exclude={.git,.gitignore,install.sh,upload_configuration.sh,configure.sh} ./ ant:/shared/public/home_config
+rsync -a -v -e ssh --exclude={.git,.gitignore,install.sh,upload_configuration.sh,configure.sh,config_commit} ./ ant:/shared/public/home_config
